e29fd2a9528b8c57f5d6dc7a6ee0c3394b0aa572
Jhon Rivera <jhonariverab@gmail.com>
2024-05-28 15:23:59 -0400

    sizes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,21 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      screens:{
+        'mobile': '375px',
+        'desktop': '1440px',
+        'ultra-desktop': '1650px'
+      },
       colors: {
-        "Lime Green": "hsl(163, 72%, 41%)",
-        "Bright Red": "hsl(356, 69%, 56%)",
+        "Lime-Green": "hsl(163, 72%, 41%)",
+        "Bright-Red": "hsl(356, 69%, 56%)",
         "Facebook": "hsl(208, 92%, 53%)",
         "Twitter": "hsl(203, 89%, 53%)",
         "YouTube": "hsl(348, 97%, 39%)",
         "Toggle-light": "hsl(230, 22%, 74%)",
         // Dark
-        "Very_Dark_Blue": "hsl(230, 17%, 14%)",
-        "Very_Dark_Blue-Top": "hsl(232, 19%, 15%)",
+        "Very-Dark-Blue": "hsl(230, 17%, 14%)",
+        "Very-Dark-Blue-Top": "hsl(232, 19%, 15%)",
         "Dark-Desaturated-Blue": "hsl(228, 28%, 20%)",
         "Desaturated-Blue": "hsl(228, 34%, 66%)",
         "White": "hsl(0, 0%, 100%)",
